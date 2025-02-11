693c66b68730e22e334bfd75675844bf84ecf26e
Lucas Silva <kucasss19@gmail.com>
2025-02-11 14:57:31 -0300

    add interrupção botão a para apagar leds

--- main.c
+++ main.c
@@ -15,6 +15,11 @@ const uint pino_botao_joystick = 22;
 
 ssd1306_t display; // variavel de controle do display
 
+static volatile bool desligar_leds = false;
+
+bool repeating_timer_callback(struct repeating_timer *t); // prototipo da função para o timer
+void gpio_irq_handler(uint gpio, uint32_t events); // prototipo da função para tratar a interrupção dos botoes
+
 int main()
 {
 
@@ -28,8 +33,33 @@ int main()
 
     stdio_init_all();
 
+    gpio_set_irq_enabled_with_callback(pino_botao_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler); // botao A
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+
+/*
+* Função para tratar a interrupção dos botão a
+*/
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    desligar_leds = !desligar_leds;
+    
+    if (desligar_leds) {
+
+        pwm_set_gpio_level(pino_led_vermelho, 0);
+        pwm_set_gpio_level(pino_led_azul, 0);
+        gpio_put(pino_led_verde, 0);
+    }
+
+}
+
+
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    // TODO: Implement this function
+    return true;
 }
