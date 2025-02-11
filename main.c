307003446a2122a18585aa8965c068cd0718c2dc
Lucas Silva <kucasss19@gmail.com>
2025-02-11 14:39:14 -0300

    add pinagem perifericos

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "ssd1603.h"
+#include "setup.h"
 
+const uint pino_led_vermelho = 13;
+const uint pino_led_azul = 12;
+const uint pino_led_verde = 11;
+const uint pino_botao_a = 5;
+const uint pino_botao_b = 6;
+const uint pino_vrx = 27;
+const uint pino_vry = 26;
+const uint pino_botao_joystick = 22;
 
+ssd1603 display;
 
 int main()
 {
